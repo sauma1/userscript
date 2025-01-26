3165e8be52dad201c3291d4b55a321b61f46d0e3
sauma1 <81866200+sauma1@users.noreply.github.com>
2025-01-26 23:23:04 +0700

    Create bypass-shield.user.js

---
+++ bypass-shield.user.js
@@ -0,0 +1,263 @@
+// ==UserScript==
+// @name          BYPASS SHIELDTEAM || SHINCHAN
+// @namespace     bypass.web.id
+// @version       1
+// @author        bypass.web.id
+// @description   Bypass ad-links using the bypass.web.id API and get to your destination without ads!
+// @match         *://mega-guy.com/*
+// @match         *://loot-link.com/*
+// @match         *://best-links.org/*
+// @match         *://loot-links.com/*
+// @match         *://megaspremium.com/*
+// @match         *://lootdest.com/*
+// @match         *://direct-links.net/*
+// @match         *://risquemega.com/*
+// @match         *://onlyfriends.club/*
+// @match         *://onepiecered.co/*
+// @match         *://multileaks.com/*
+// @match         *://luvsquad-links.com/*
+// @match         *://lootdest.org/*
+// @match         *://free-leaks.com/*
+// @match         *://goldmega.online/*
+// @match         *://realiukzemydre.com/*
+// @match         *://kmendation.com/*
+// @match         *://lootlinks.co/*
+// @match         *://onlyshare.info/*
+// @match         *://leakplugs.com/*
+// @match         *://nswfbox.com/*
+// @match         *://eofmukindwo.com/*
+// @match         *://op-packs.com/*
+// @match         *://thedetective.online/*
+// @match         *://forlinkmeg.com/*
+// @match         *://thegoatpack.org/*
+// @match         *://of-leaks.xyz/*
+// @match         *://links.spacebin.in/*
+// @match         *://supermeg.com/*
+// @match         *://onlyfanessereloaded.com/*
+// @match         *://fanzleaks.com/*
+// @match         *://cemendemons.com/*
+// @match         *://premiummegaz.com/*
+// @match         *://thepremium.online/*
+// @match         *://pasteebins.com/*
+// @match         *://baddiezcentral.com/*
+// @match         *://megazone.website/*
+// @match         *://leak-pragmatic.com/*
+// @match         *://megadumpz.com/*
+// @match         *://thhaven.net/*
+// @match         *://meg4fans.com/*
+// @match         *://depravityweb.co/*
+// @match         *://discordlink.cc/*
+// @match         *://megashub.co/*
+// @match         *://streamergirls.org/*
+// @match         *://your-leaks.com/*
+// @match         *://onlylinksmegas.xyz/*
+// @match         *://pypy.spacebin.in/*
+// @match         *://pypy.in/*
+// @match         *://luvsquad-links.cmo/*
+// @match         *://beast.net.in/*
+// @match         *://hook.beast.net.in/*
+// @match         *://all-fans.online/*
+// @match         *://fansmega.com/*
+// @match         *://worldpacks.co/*
+// @match         *://night-hub.online/*
+// @match         *://dailyofleaks.com/*
+// @match         *://pkofs.com/*
+// @match         *://offree90.com/*
+// @match         *://megadropz.com/*
+// @match         *://onlyfunlink.com/*
+// @match         *://direct-links.org/*
+// @match         *://leaks4you.com/*
+// @match         *://onlylinksmegas.com/*
+// @match         *://direct-link.net/*
+// @match         *://downbadleaks.com/*
+// @match         *://onlyfanscloud.com/*
+// @match         *://missleakz.com/*
+// @match         *://leakszone.online/*
+// @match         *://links-loot.com/*
+// @match         *://holyfanslinks.com/*
+// @match         *://utopianleaks.com/*
+// @match         *://megavip.store/*
+// @match         *://drlinker.com/*
+// @match         *://baddiesheaven.com/*
+// @match         *://of4lm-links.com/*
+// @match         *://holedonly.store/*
+// @match         *://lootlinks.com/*
+// @match         *://free-content.pro/*
+// @match         *://milky-center.com/*
+// @match         *://megaofs.com/*
+// @match         *://bleleadersto.com/*
+// @match         *://link-target.org/*
+// @match         *://daughablelea.com/*
+// @match         *://mymegalinks.com/*
+// @match         *://heroslut.com/*
+// @match         *://tonordersitye.com/*
+// @match         *://vip-linknetwork.com/*
+// @match         *://birdbiss.com/*
+// @match         *://loot-labs.com/*
+// @match         *://lootlabs.com/*
+// @match         *://link-hub.net/*
+// @match         *://locconnect.com/*
+// @match         *://premiumstashdrop.com/*
+// @match         *://lootdest.info/*
+// @match         *://of-area.com/*
+// @match         *://link-target.net/*
+// @match         *://megalnk.com/*
+// @match         *://lootlink.org/*
+// @match         *://nsfwcherry.com/*
+// @match         *://cherrypacks.online/*
+// @match         *://hotstars-leaks.com/*
+// @match         *://onlymega.co/*
+// @match         *://hanimeturks.com/*
+// @match         *://dailyleakz.com/*
+// @match         *://content-hub.club/*
+// @match         *://dailyadultmegas.com/*
+// @match         *://oui-chu.com/*
+// @match         *://megalinks.one/*
+// @match         *://starleakz.com/*
+// @match         *://babeslink.click/*
+// @match         *://ofgirls3x.com/*
+// @match         *://pnp-drops.me/*
+// @match         *://nsfw-paradise.club/*
+// @match         *://key-access.co/*
+// @match         *://leaksmix.com/*
+// @match         *://sensual-leaks.com/*
+// @match         *://ofhub-leaks.com/*
+// @match         *://onlyforfan.online/*
+// @match         *://sweetjuice-mega.com/*
+// @match         *://crip-hub.com/*
+// @match         *://leaksfreeday.com/*
+// @match         *://thotshaven.online/*
+// @match         *://mega-leaked.com/*
+// @match         *://beautifulgirls.social/*
+// @match         *://tavernleaks.com/*
+// @match         *://nudeleaksteenz.com/*
+// @match         *://slutywet.com/*
+// @match         *://leakutopia.site/*
+// @match         *://secret-packs.com/*
+// @match         *://attiktok22.com/*
+// @match         *://pancakes-leaks.com/*
+// @match         *://diamond-leaks.com/*
+// @match         *://lewd-leaks.com/*
+// @match         *://ftbleaks.net/*
+// @match         *://thepytheaven.org/*
+// @match         *://of-region.online/*
+// @match         *://admiregirls-byme.com/*
+// @match         *://mzehoney12.com/*
+// @match         *://herplace.online/*
+// @match         *://megaplugleaks.com/*
+// @match         *://of-kingdom.com/*
+// @match         *://onlyfansmegafolder.com/*
+// @match         *://links.spacebin.in/*
+// @match         *://adfoc.us/*
+// @match         *://boost.ink/*
+// @match         *://cuty.io/*
+// @match         *://www.cuty.io/*
+// @match         *://cety.app/*
+// @match         *://www.cety.app/*
+// @match         *://linkvertise.com/*/*
+// @match         *://mboost.me/*
+// @match         *://bst.gg/*
+// @match         *://booo.st/*
+// @match         *://mendationforc.info/*
+// @match         *://paster.so/*
+// @match         *://paster.gg/*
+// @match         *://rekonise.com/*
+// @match         *://social-unlock.com/*
+// @match         *://www.social-unlock.com/*
+// @match         *://socialwolvez.com/*
+// @match         *://sub2get.com/*
+// @match         *://www.sub2get.com/*
+// @match         *://sub2unlock.com/*
+// @match         *://sub2unlock.io/*
+// @match         *://sub2unlock.net/*
+// @match         *://sub2unlock.online/*
+// @match         *://sub2unlock.top/*
+// @match         *://sub4unlock.pro/*
+// @match         *://sub4unlock.com/*
+// @match         *://sub4unlock.io/*
+// @match         *://subfinal.com/*
+// @match         *://unlocknow.net/*
+// @match         *://v.gd/*
+// @match         *://work.ink/*
+// @match         *://www.work.ink/*
+// @match         *://workink.net/*
+// @match         *://r.work.ink/*
+// @match         *://workink.one/*
+// @match         *://workink.me/*
+// @match         *://ytsubme.com/*
+// @match         *://www.ytsubme.com/*
+// @match         *://esohasl.net/*
+// @match         *://rbscripts.net/*
+// @match         *://link.rbscripts.net/*
+// @homepageURL   https://bypass.web.id
+// @run-at document-start
+// ==/UserScript==
+
+(async () => {
+    const currentUrl = window.location.href;
+    console.log("Current Window URL:", currentUrl);
+    if (!currentUrl) {
+        console.log("URL tidak ditemukan.");
+        document.body.innerHTML = `<h1>Error</h1><p>URL tidak ditemukan di tab browser.</p>`;
+        return;
+    }
+    if (!currentUrl.includes("linkvertise")) {
+        console.log("URL bukan dari Linkvertise.");
+        document.body.innerHTML = `<h1>Error</h1><p>Hanya URL dari Linkvertise yang bisa diproses.</p>`;
+        return;
+    }
+
+    try {
+        const apiUrl = `https://api.bypass.web.id/api/bypass?url=${currentUrl}&method=anyreq`;
+        console.log("API Request URL:", apiUrl);
+        const apiResponse = await fetch(apiUrl, { method: 'GET' });
+        const apiData = await apiResponse.json();
+        console.log("API Response Data:", apiData);
+
+        if (apiData.taskId) {
+            console.log("Task ID ditemukan:", apiData.taskId);
+
+            let statusData;
+            let retries = 0;
+            const maxRetries = 100;
+            do {
+                const statusUrl = `https://api.bypass.web.id/api/bypass/status/${apiData.taskId}`;
+                console.log("Status API Request URL:", statusUrl);
+                const statusResponse = await fetch(statusUrl, { method: 'GET' });
+                statusData = await statusResponse.json();
+                console.log("Status API Response Data:", statusData);
+                if (statusData.status === "processing") {
+                    console.log("Status masih processing, mencoba lagi...");
+                }
+
+                retries++;
+                if (retries >= maxRetries) {
+                    console.log("Max retries reached, exiting...");
+                    break;
+                }
+
+            } while (statusData.status === "processing");
+
+            if (statusData.status === 'completed') {
+                console.log("URL Bypass berhasil:", statusData.result);
+                const encodedUrl = encodeURIComponent(statusData.result);
+                console.log("Encoded URL:", encodedUrl);
+                location.href = `${currentUrl}&redirect=${encodedUrl}`;
+                location.href = statusData.result;
+                return;
+            } else {
+                console.log("Bypass gagal:", statusData.message || "Unknown error.");
+                document.body.innerHTML = `<h1>Redirect Failed</h1><p>${statusData.message || "Unknown error."}</p>`;
+            }
+        } else {
+            console.log("Task ID tidak ditemukan di API Response.");
+            document.body.innerHTML = `<h1>API Error</h1><p>Task ID tidak ditemukan.</p>`;
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        document.body.innerHTML = `<h1>Error</h1><p>${error.message}</p>`;
+    }
+})();
+
+
+
